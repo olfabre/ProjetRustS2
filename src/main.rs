f2d396a71bdad605c450457a9ac65a8ccd703c00
Tiago <tiagofdev@gmail.com>
2025-02-26 16:26:32 +0100

    ok

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use serde:: {Deserialize, Serialize};
+use std::fs;
+use std::io;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Joueur {
+    r#type: String,
+    r#position: String,
+}
+// main change
+
+fn main() {
+    println!("Hello, world!");
+
+    let json_str = fs::read_to_string("src/data.json").expect("Can't read file");
+    let joueur: Joueur = serde_json::from_str(&json_str).expect("Can't parse json");
+
+    println!("{}", joueur.r#type);
+
+}
